eee26c501ca8925a0e62b68a973a279b08b490c0
Laura Antunez <lauraantunez024@gmail.com>
2023-09-18 17:03:06 -0400

    trying to push to vercel

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,6 +61,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    publicPath: 'dist',
     extend(config, ctx) {
       config.module.rules.push({
         test: /\.(mp3)$/,
